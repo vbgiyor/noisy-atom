0870eba336a105f99854410c605a42845b137c30
Shekhar Bhosale <redstar@Redstar-X.local>
2025-03-06 11:00:00 +0530

    Remove files

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("search").addEventListener("input", function() {
-    let query = this.value.toLowerCase();
-    console.log("Searching for:", query); // To be improved later
-});
